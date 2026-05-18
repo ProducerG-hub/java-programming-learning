392c555237d1bf06ccd39733c2198cce6a7fae94
gwamaka mwakabuta <gwamakamwakabuta@gmail.com>
2026-05-18 21:15:36 +0300

    initial commit

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Am Producer G!");
+        System.out.println("This is my first Java program.");
+    }
+}
